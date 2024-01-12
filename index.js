7101bd54b6798bb37cc98cada9a8225ec9334ba4
JustinLieu06 <justinlieu06@gmail.com>
2024-01-12 14:53:24 -0800

    added REST CRUD features

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -32,8 +34,40 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id)) 
+    : 0
+
+    const person = request.body
+    person.id = maxId + 1
+
+    persons = persons.concat(person);
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    // Upon a success, 204 is returned
+    // Apps either return 204 or 404 upon a fail. Let's just return 204 to keep it simple
+    response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
-    response.json(`Phonebook has info for ${persons.length} people<br/> `)
+    response.json(`Phonebook has info for ${persons.length} people`)
 })
 
 const PORT = 3001
