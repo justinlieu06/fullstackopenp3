3fcac4922b7b125ad1a595e2d42bfe575292b45b
JustinLieu06 <justinlieu06@gmail.com>
2024-01-19 14:35:28 -0800

    working on having mongo script work still

--- mongo.js
+++ mongo.js
@@ -1,3 +1,4 @@
+// should be able to run this script w/ node mongo.js <password>
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
@@ -6,16 +7,16 @@ if (process.argv.length<3) {
 }
 
 const password = process.argv[2]
-
-const url =
-  `mongodb+srv://justinlieu06:${password}>@cluster0.zjvlpvz.mongodb.net/?/?retryWrites=true&w=majority`
+// must update this url based on which cluster connecting to
+const url = `mongodb+srv://testUser:${password}>@cluster0.zjvlpvz.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
+// create schema
 const personSchema = new mongoose.Schema({
   name: String,
-  number: Boolean,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -25,7 +26,9 @@ const person = new Person({
   number: '17459814820',
 })
 
+// save to schema
 person.save().then(result => {
   console.log('person saved!')
   mongoose.connection.close()
 })
+
