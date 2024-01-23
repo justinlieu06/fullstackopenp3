a82a9db8f076a04642eb68d480fa0f428d6a46dd
JustinLieu06 <justinlieu06@gmail.com>
2024-01-22 17:16:59 -0800

    updated mongo script to take in person params

--- mongo.js
+++ mongo.js
@@ -7,6 +7,8 @@ if (process.argv.length<3) {
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 // must update this url based on which cluster connecting to
 const url = `mongodb+srv://justinlieu06:${password}@cluster0.i35aru0.mongodb.net/?retryWrites=true&w=majority`
 
@@ -22,22 +24,28 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
+// const person = new Person({
+//   name: 'Mongo Test2',
+//   number: '17459814820',
+// })
+
 const person = new Person({
-  name: 'Mongo Test2',
-  number: '17459814820',
-})
+    name: name,
+    number: number,
+  })
 
 /* For creating a new person */
-// node mongo.js <password>
-// person.save().then(result => {
-// //   console.log(result); // uninteresting for saving one obj
-//   console.log('person saved!')
-//   mongoose.connection.close()
-// })
+// Previously: node mongo.js <password>
+// Currently: node mongo.js <password> Anna 040-1234556
+person.save().then(result => {
+//   console.log(result); // uninteresting for saving one obj
+  console.log('person saved!')
+  mongoose.connection.close()
+})
 
 //node mongo.js <password> "Arto Vihavainen" 045-1232456
 Person.find({}).then(result => {
-    result.forEach(pereson => {
+    result.forEach(person => {
       console.log(person)
     })
     mongoose.connection.close()
