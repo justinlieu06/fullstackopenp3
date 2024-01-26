7434344e49e64c4c8ce96e287cc58387635e3987
JustinLieu06 <justinlieu06@gmail.com>
2024-01-26 15:01:08 -0800

    added error handling to getting by person id

--- index.js
+++ index.js
@@ -64,7 +64,15 @@ app.get('/api/persons/:id', (request, response) => {
     // }
 
     Person.findById(request.params.id).then(person=> {
-        response.json(person)
+        if (person){
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).end()
     })
 })
 
