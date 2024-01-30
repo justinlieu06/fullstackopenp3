ad782c05139a929ace60460b5161173dd5726553
JustinLieu06 <justinlieu06@gmail.com>
2024-01-30 10:51:18 -0800

    added put to update persons obj

--- index.js
+++ index.js
@@ -147,6 +147,22 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
+// update persons
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+        response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 app.get('/info', (request, response) => {
     var currentdate = new Date(); 
     var datetime = "Called on: " + currentdate.getDate() + "/"
