4e9b988c4b0b5e057c2d949884f8fc2b223bac24
JustinLieu06 <justinlieu06@gmail.com>
2024-02-05 13:30:13 -0800

    added er rhandling to person save

--- index.js
+++ index.js
@@ -139,6 +139,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
